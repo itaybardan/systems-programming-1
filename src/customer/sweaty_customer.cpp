d9a95b6112205c38b9f0819c31f51bdcabed5a85
oren <orenpartovi@gmail.com>
2021-11-17 00:34:59 +0200

    order func fixed

--- src/customer/sweaty_customer.cpp
+++ src/customer/sweaty_customer.cpp
@@ -1,4 +1,20 @@
-//
-// Created by itay on 12/11/2021.
-//
+#include "../../include/customer.h"
 
+SweatyCustomer::SweatyCustomer(std::string name, int id) : Customer(name, id), swt(name) ,id(id){
+
+}
+
+std::vector<int> SweatyCustomer::order(const std::vector<Workout> &workout_options) {
+    std::vector<int> orders;
+    //this customer orders all of CARDIO workouts.
+    for ( int i = 0 ; i < static_cast<int>(workout_options.size()) ; i++){
+        if (workout_options[i].getType() == CARDIO){
+            orders.push_back(workout_options[i].getId());
+        }
+    }
+    return orders;
+}
+
+std::string SweatyCustomer::toString() const {
+    return "I am a sweaty customer"
+}
