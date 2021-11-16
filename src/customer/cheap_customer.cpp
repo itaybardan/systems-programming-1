615decc06cd2a6f452697bbc4f42a5a054672d5f
oren <orenpartovi@gmail.com>
2021-11-17 00:42:10 +0200

    cheap customer order done

---
+++ src/customer/cheap_customer.cpp
@@ -0,0 +1,28 @@
+
+#include <climits>
+#include "../../include/customer.h"
+
+CheapCustomer::CheapCustomer(std::string name, int id) : Customer(name, id),chp(name), id(id) {
+
+}
+
+std::vector<int> CheapCustomer::order(const std::vector<Workout> &workout_options) {
+    std::vector<int> orders;
+    int min_id = INT_MAX;
+    int min_price = INT_MAX;
+    for(int i = 0 ; i < static_cast<int>(workout_options.size()) ; i++){
+        // always take the smallest ID of the cheapest workout.
+        if(min_price > workout_options[i].getPrice())
+        {
+            min_price = workout_options[i].getPrice();
+            min_id = i;
+        }
+    }
+    //return a vector with the cheapest workout.
+    orders.push_back(workout_options[min_id].getId());
+    return orders;
+}
+
+std::string CheapCustomer::toString() const {
+    return "I am a cheap customer";
+}
