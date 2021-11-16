d837433629f0a3ce308a36be0f2674022faeea2e
Itay Bardan <itay.bardan@orchestra.group>
2021-11-16 22:13:21 +0200

    comment open trainer

--- src/action/open_trainer.cpp
+++ src/action/open_trainer.cpp
@@ -8,25 +8,27 @@ OpenTrainer::OpenTrainer(int id, std::vector<Customer *> &customersList) : train
 }
 
 void OpenTrainer::act(Studio &studio) {
-    Trainer* tempTrainer = studio.getTrainer(trainerId);
-    //the if consists of three "ors":
-    //check if the trainer's workout session is already open or there exists such trainer
-    //or if there is spots in Trainer's capacity.
-    if (tempTrainer == nullptr || tempTrainer->isOpen() == true ||customers.size() > tempTrainer->getCapacity())
-        error("Workout session does not exist or is already open.");
-    else{
-        //open a workout session.
-        tempTrainer->openTrainer();
-        //update the customers vector
-        for (int (i) = 0 ; (i) < customers.size() ; (i)++) {
-            tempTrainer->addCustomer(customers[i]);
-        }
-        // call the function complete in BaseAction.
-        complete();
-    }
+    ;
+//    Trainer* tempTrainer = studio.getTrainer(trainerId);
+//    //the if consists of three "ors":
+//    //check if the trainer's workout session is already open or there exists such trainer
+//    //or if there is spots in Trainer's capacity.
+//    if (tempTrainer == nullptr || tempTrainer->isOpen() == true ||customers.size() > tempTrainer->getCapacity())
+//        error("Workout session does not exist or is already open.");
+//    else{
+//        //open a workout session.
+//        tempTrainer->openTrainer();
+//        //update the customers vector
+//        for (int (i) = 0 ; (i) < customers.size() ; (i)++) {
+//            tempTrainer->addCustomer(customers[i]);
+//        }
+//        // call the function complete in BaseAction.
+//        complete();
+//    }
 }
 
 std::string OpenTrainer::toString() const {
+    return std::string();
     return "Trainer " + std::to_string(trainerId) + " is open and added " + std::to_string(customers.size()) +" customers";
 
 }
