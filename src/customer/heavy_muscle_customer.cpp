7ff6167b2d016354151cf31bf72c50630a8b346e
oren <orenpartovi@gmail.com>
2021-11-18 17:30:41 +0200

    mcl fixed

--- src/customer/heavy_muscle_customer.cpp
+++ src/customer/heavy_muscle_customer.cpp
@@ -1,28 +1,26 @@
-#include <algorithm>
-#include <climits>
+
 #include "../../include/customer.h"
 
 HeavyMuscleCustomer::HeavyMuscleCustomer(std::string name, int id) : Customer(name, id), mcl(name), id(id) {
 
 }
-//NOT DONE
+
 std::vector<int> HeavyMuscleCustomer::order(const std::vector<Workout> &workout_options) {
     std::vector<int> orders;
-    //making a vector of ID's and Prices [i|p|i|p|i|p...]
+    std::vector<Workout> temp_workout_options = workout_options;
     for (int i = 0; i < static_cast<int>(workout_options.size()); i++) {
         if (workout_options[i].getType() == ANAEROBIC) {
+            //the workouts are sorted by price from the most expensive to the cheapest
+            //therefore its ok to push right away.
             orders.push_back(workout_options[i].getId());
-            orders.push_back(workout_options[i].getPrice());
         }
     }
-    //inserting only the prices
-//    for (int i = 1; i < static_cast<int>(pre_orders.size()); i+=2) {
-
-
-
-
-
-
 
 return orders;
 }
+
+
+std::string HeavyMuscleCustomer::toString() const {
+    return "Im a Heavy Muscle Customer";
+}
+
