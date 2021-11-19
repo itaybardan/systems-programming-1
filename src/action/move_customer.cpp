fe9149071d8ebe2b865d11c0bf67782560e90f55
oren <orenpartovi@gmail.com>
2021-11-19 15:54:42 +0200

    if org_trainer is empty, close workout

--- src/action/move_customer.cpp
+++ src/action/move_customer.cpp
@@ -1,13 +1,19 @@
-#include "../../include/action.h"
+#include "../../include/studio.h"
 
 MoveCustomer::MoveCustomer(int src, int dst, int customerId) : srcTrainer(src), dstTrainer(dst), id(customerId) {
 
 }
 
 void MoveCustomer::act(Studio &studio) {
+Trainer *origin_trainer = studio.getTrainer(srcTrainer);
+Trainer *dst_trainer = studio.getTrainer(dstTrainer);
+Customer *temp_customer = origin_trainer->getCustomer(id);
 
+//closes the origin trainer workout session if there are no customers left in his workout session
+if(origin_trainer->getCustomers().empty())
+    origin_trainer->closeTrainer();
 }
 
 std::string MoveCustomer::toString() const {
-    return std::string();
+    return "";
 }
